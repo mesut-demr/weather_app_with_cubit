ae6a972b96df012d010895d23da2458c4aaedba9
mesut-demr <mesutdr82@gmail.com>
2024-01-08 09:29:24 +0300

    updated weather_service

--- lib/serivce/weather_service.dart
+++ lib/serivce/weather_service.dart
@@ -1,9 +1,10 @@
 import 'package:dio/dio.dart';
+import 'package:weather_app_with_cubit/model/weather_model.dart';
 
 class WeatherApiService {
   final Dio _dio = Dio();
 
-  Future<Weather> fetchWeatherForUserLocation() async {
+  Future<WeatherModel> fetchWeatherForUserLocation() async {
     try {
       Response response = await _dio
           .get('https://api.weatherapi.com/v1/current.json', queryParameters: {
@@ -11,7 +12,34 @@ class WeatherApiService {
         'q': '',
       });
 
-      return Weather
-    } catch (e) {}
+      return WeatherModel(
+        city: response.data['location']['name'],
+        condition: response.data['current']['condition']['text'],
+        temperature: response.data['current']['temp_c'],
+      );
+    } catch (e) {
+      throw Exception('Hava Durumu Bilgisi Alınamadı: $e');
+    }
+  }
+
+  Future<WeatherModel> fetchWeatherForCity(String city) async {
+    try {
+      Response response = await _dio
+          .get('https://api.weatherapi.com/v1/current.json', queryParameters: {
+        'key': '',
+        'q': city,
+      });
+
+      return WeatherModel(
+        city: response.data['location']['name'],
+        condition: response.data['current']['condition']['text'],
+        temperature: response.data['current']['temp_c'],
+      );
+    } catch (e) {
+      throw Exception('Hava Durumu Bilgisi Alınamadaı: $e');
+    }
   }
 }
+
+
+//try üstüne tanımlama yapılabilir
